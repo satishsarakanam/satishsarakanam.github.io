febb7e2da7e9337ed89431375fff6ca7c00098e7
Sridevi Sarakanam <sridevisarakanam@MacBookAir.lan>
2026-04-08 22:25:34 -0400

    More changes

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
-rm -r ./dist
-mkdir dist
-cp satishsarakanam.pdf ./dist/
-cp satishsarakanamdocx.docx ./dist/
-cp -r certificates/* ./dist
-cp -r images css libs ./dist/
-cp index.html favicon.png httpsServer.js ./dist/
-uglifyjs js/*.js -o ./dist/bundle.min.js -c -m
-uglifycss ./css/*.css > ./dist/css/style.min.css
-awk '{gsub("<script src=\"./js/templates.js\"></script>","");print}' ./dist/index.html > ./dist/tmpfile.html && cat ./dist/tmpfile.html > ./dist/index.html
-awk '{gsub("<script src=\"./js/global.js\"></script>","");print}' ./dist/index.html > ./dist/tmpfile.html && cat ./dist/tmpfile.html > ./dist/index.html
-awk '{gsub("<script src=\"./js/script.js\"></script>","<script src=\"./bundle.min.js?v='$(date +"%s")'\"></script>");print}' ./dist/index.html > ./dist/tmpfile.html && cat ./dist/tmpfile.html > ./dist/index.html
-awk '{gsub("<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.css\">","<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.min.css?v='$(date +"%s")'\">");print}' ./dist/index.html > ./dist/tmpfile.html && cat ./dist/tmpfile.html > ./dist/index.html
-rm ./dist/tmpfile.html
-rm ./dist/css/style.css
+rm -r ./docs
+mkdir docs
+cp satishsarakanam.pdf ./docs/
+cp satishsarakanamdocx.docx ./docs/
+cp -r certificates/* ./docs
+cp -r images css libs ./docs/
+cp index.html favicon.png httpsServer.js ./docs/
+uglifyjs js/*.js -o ./docs/bundle.min.js -c -m
+uglifycss ./css/*.css > ./docs/css/style.min.css
+awk '{gsub("<script src=\"./js/templates.js\"></script>","");print}' ./docs/index.html > ./docs/tmpfile.html && cat ./docs/tmpfile.html > ./docs/index.html
+awk '{gsub("<script src=\"./js/global.js\"></script>","");print}' ./docs/index.html > ./docs/tmpfile.html && cat ./docs/tmpfile.html > ./docs/index.html
+awk '{gsub("<script src=\"./js/script.js\"></script>","<script src=\"./bundle.min.js?v='$(date +"%s")'\"></script>");print}' ./docs/index.html > ./docs/tmpfile.html && cat ./docs/tmpfile.html > ./docs/index.html
+awk '{gsub("<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.css\">","<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.min.css?v='$(date +"%s")'\">");print}' ./docs/index.html > ./docs/tmpfile.html && cat ./docs/tmpfile.html > ./docs/index.html
+rm ./docs/tmpfile.html
+rm ./docs/css/style.css
